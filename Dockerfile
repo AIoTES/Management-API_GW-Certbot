661dfc570b1002f491f5d8642df2652f522c8082
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2019-11-08 16:56:40 +0000

    fixed issues

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,6 @@
 FROM certbot/certbot:v0.40.1
 
-RUN pip install schedule
-
 COPY run.py /run.py
+RUN pip install schedule && chmod +x /run.py
 
-EXPOSE 80
-VOLUME ["/etc/letsencrypt"]
-
-CMD ["/run.py"]
+ENTRYPOINT ["python","/run.py"]
--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import schedule
 import time
+import datetime
 from os import environ as env
 from subprocess import run
 
 def certonly():
-    print("Configuring or renewing certificate")
-    print run(["certbot", "certonly","-n", "--standalone", "--cert-name", "aiotes", "--keep-until-expiring","--renew-with-new-domains","--agree-tos" "-d", env.get('AIOTES_HOSTNAME')])
+    print (datetime.datetime.now()," Configuring or renewing certificate")
+    print (run(["certbot", "certonly","-n", "--standalone", "--cert-name", "aiotes", "--keep-until-expiring","--renew-with-new-domains","--agree-tos","--email" , env.get('AIOTES_SYSADMIN_EMAIL'), "-d", env.get('AIOTES_HOSTNAME')]))
+#    print (run(["ls", "-lhaR", "/etc/letsencrypt"]))
 
+print(datetime.datetime.now()," Initialising Certbot pySchedule")
 certonly()
 schedule.every().monday.do(certonly)
 
