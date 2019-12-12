75c2d82b4c2bd01773b867ff91dfa0407508dcdb
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2019-12-12 13:34:49 +0100

    updated parameters of Self-Signed Certs.

--- run.py
+++ run.py
@@ -16,18 +16,18 @@ def create_self_signed_cert():
     KEY_FILE = eg_certs+cert_files[0]
     # create a key pair
     k = crypto.PKey()
-    k.generate_key(crypto.TYPE_RSA, 1024)
+    k.generate_key(crypto.TYPE_RSA, 4096)
 
     # create a self-signed cert
     cert = crypto.X509()
     cert.get_subject().O = "AIOTES Instance"
-    cert.get_subject().CN = "localhost"
+    cert.get_subject().CN = os.getenv('AIOTES_HOSTNAME','localhost')
     cert.set_serial_number(1000)
     cert.gmtime_adj_notBefore(0)
     cert.gmtime_adj_notAfter(10*365*24*60*60)
     cert.set_issuer(cert.get_subject())
     cert.set_pubkey(k)
-    cert.sign(k, 'sha1')
+    cert.sign(k, 'sha256')
 
     f=open(CERT_FILE, "wb")
     f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
