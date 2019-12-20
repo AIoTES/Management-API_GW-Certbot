54dde86d473a045247251a3a42829bb55d82042c
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2019-12-20 14:13:39 +0100

    Added test for express gateway self-signed detection mechanism.

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#check if certificates are self-signed
+if [ -f "/var/lib/certs/live/aiotes/self-signed" ]; then
+    echo "self-signed certificate detected"
+	export NODE_EXTRA_CA_CERT=/var/lib/certs/live/aiotes/cert.pem
+	echo "NODE_EXTRA_CA_CERT="$(printenv NODE_EXTRA_CA_CERT)
+fi
+mkdirs /var/lib/certs/live/aiotes/
+touch /var/lib/certs/live/aiotes/self-signed
+#check if certificates are self-signed
+if [ -f "/var/lib/certs/live/aiotes/self-signed" ]; then
+    echo "self-signed certificate detected"
+	export NODE_EXTRA_CA_CERT=/var/lib/certs/live/aiotes/cert.pem
+	echo "NODE_EXTRA_CA_CERT="$(printenv NODE_EXTRA_CA_CERT)
+fi
